d743d93dbacfd2c4825e0425a08ea5fd6be6641f
Cedric Caparas <caparas2@myumanitoba.ca>
2026-05-29 18:22:21 -0500

    #60 Fix task handle name mismatch

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -53,8 +53,8 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-extern osThreadId_t flowmeterHandle;
-extern osThreadId_t wheelSpeedHandle;
+extern osThreadId_t readFlowmeterHandle;
+extern osThreadId_t readWheelSpeedHandle;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -214,8 +214,8 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
   if (htim->Instance == TIM6) {
 	  BaseType_t higherPriorityTaskWoken = pdFALSE;
 
-	  vTaskNotifyGiveFromISR((TaskHandle_t)flowmeterTaskHandle,  &higherPriorityTaskWoken);
-	  vTaskNotifyGiveFromISR((TaskHandle_t)wheelSpeedTaskHandle, &higherPriorityTaskWoken);
+	  vTaskNotifyGiveFromISR((TaskHandle_t)readFlowmeterHandle,  &higherPriorityTaskWoken);
+	  vTaskNotifyGiveFromISR((TaskHandle_t)readWheelSpeedHandle, &higherPriorityTaskWoken);
 	  // If a woken task is higher priority than what we interrupted,
 	  // switch to it immediately when the ISR returns.
 	  portYIELD_FROM_ISR(higherPriorityTaskWoken);
